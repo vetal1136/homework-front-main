058f4cb3581c54d2c1a20f830a96efe8062dc6ea
vetal1136 <komrad188@yandex.ru>
2024-04-11 12:02:18 +0300

    homework 3

--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -14,27 +14,27 @@ export const HW1 = () => {
   // ❗ id на элементах не удаляем и не изменяем! Они нужны для автотестов.
   // Если нужно подставить элемент массива в id, то подставляем по аналогии с другими элементами!
 
-	const users = [
-		{id: 1, name: 'John', age: 25, address: {street: '123 Main St', city: 'New York'}},
-		{id: 2, name: 'Alice', age: 30, address: {street: '456 Elm St', city: 'San Francisco'}},
-		{id: 3, name: 'Bob', age: 35, address: {street: '789 Oak St', city: 'Seattle'}},
-		{id: 4, name: 'John', age: 44, address: {street: '123 Maple Ave', city: 'Dallas'}},
-		{id: 5, name: 'Mary', age: 13, address: {street: '456 Elm St', city: 'Los Angeles'}},
-		{id: 6, name: 'James', age: 18, address: {street: '987 Pine Rd', city: 'Dallas'}},
-		{id: 7, name: 'Jennifer', age: 21, address: {street: '654 Birch Ct', city: 'Seattle'}},
-		{id: 8, name: 'Robert', age: 4, address: {street: '876 Spruce Way', city: 'San Francisco'}},
-		{
-			id: 9,
-			name: 'Jessica',
-			age: 12,
-			address: {street: '543 Willow Dr', city: 'San Francisco'},
-		},
-		{id: 10, name: 'Emily', age: 55, address: {street: '765 Aspen Blvd', city: 'Los Angeles'}},
-	];
+const users = [
+	{id: 1, name: 'John', age: 25, address: {street: '123 Main St', city: 'New York'}},
+	{id: 2, name: 'Alice', age: 30, address: {street: '456 Elm St', city: 'San Francisco'}},
+	{id: 3, name: 'Bob', age: 35, address: {street: '789 Oak St', city: 'Seattle'}},
+	{id: 4, name: 'John', age: 44, address: {street: '123 Maple Ave', city: 'Dallas'}},
+	{id: 5, name: 'Mary', age: 13, address: {street: '456 Elm St', city: 'Los Angeles'}},
+	{id: 6, name: 'James', age: 18, address: {street: '987 Pine Rd', city: 'Dallas'}},
+	{id: 7, name: 'Jennifer', age: 21, address: {street: '654 Birch Ct', city: 'Seattle'}},
+	{id: 8, name: 'Robert', age: 4, address: {street: '876 Spruce Way', city: 'San Francisco'}},
+	{
+		id: 9,
+		name: 'Jessica',
+		age: 12,
+		address: {street: '543 Willow Dr', city: 'San Francisco'},
+	},
+	{id: 10, name: 'Emily', age: 55, address: {street: '765 Aspen Blvd', city: 'Los Angeles'}},
+];
 
-	return (
-		<div id={'hw01'}>
-			{/*<UserList users={'НАЗВАНИЕ МАССИВА, КОТОРЫЙ МЫ ПЕРЕДАЕМ'} />*/}
-		</div>
-	);
+return (
+	<div id={'hw01'}>
+		<UserList users={users} />
+	</div>
+);
 };
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,23 +1,27 @@
 import {CurrentUser} from './CurrentUser';
 import {UsersObjectType} from './HW2';
+import {MouseEventHandler} from "react";
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+	users: UsersObjectType // пропиши типизацию
+	filterUsers:MouseEventHandler<HTMLButtonElement> // пропиши типизацию
+
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
+
+
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+			<button id={'hw02-filter-button'} onClick={ props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
+			<ul>
+			  {props.users.myFriends.map((user) => (
+			    <CurrentUser  user={user} />
+			  ))}
+			</ul>
 		</div>
 	);
 };
