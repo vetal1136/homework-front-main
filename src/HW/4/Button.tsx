1ab1404fedc68c64ef7d75d8250a35207e774aba
vetal1136 <komrad188@yandex.ru>
2024-04-18 10:34:01 +0300

    homework 4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -6,7 +6,7 @@ type ButtonPropsType = {
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
     // НУЖНО ДОПИСАТЬ
-    props.callBack
+    props.callBack()
   };
 
   return (
--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -34,7 +34,7 @@ export const HW4 = () => {
 
       <Input currentText={currentText} setCurrentText={setCurrentText} />
 
-      <Button name={"?????"} callBack={handleSave} />
+      <Button name={"кнопка"} callBack={handleSave} />
 
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
